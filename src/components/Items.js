b76e2c86037bf1e6c36478717330c84cfb380062
leesangha <sanghalee1@naver.com>
2021-09-02 20:03:21 +0900

    divide scripts

---
+++ src/components/Items.js
@@ -0,0 +1,23 @@
+import Component from "../core/Components";
+
+export default class Itesm extends Component {
+  setup() {
+    this.$state = { items: ["item1", "item2"] };
+  }
+  template() {
+    const { items } = this.$state;
+    return `
+      <ul>
+        ${items.map((item) => `<li>${item}</li>`).join("")}
+      </ul>
+      <button>추가</button>
+    `;
+  }
+  setEvent() {
+    this.$target.querySelector("button").addEventListener("click", () => {
+      const { items } = this.$state;
+      let word = `item${items.length + 1}`;
+      this.setState({ items: [...items, word] });
+    });
+  }
+}
