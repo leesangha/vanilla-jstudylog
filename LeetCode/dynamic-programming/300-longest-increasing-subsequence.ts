ab4c8cf1e10a935e8527be690012bd2d9a52ccab
leesangha <sanghalee1@naver.com>
2025-09-08 16:58:22 +0900

    solved: 300-longest-increasing-subsequence

---
+++ LeetCode/dynamic-programming/300-longest-increasing-subsequence.ts
@@ -0,0 +1,12 @@
+function lengthOfLIS(nums: number[]): number {
+  const dp = Array.from({ length: nums.length }, () => 1);
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+  return Math.max(...dp);
+}
