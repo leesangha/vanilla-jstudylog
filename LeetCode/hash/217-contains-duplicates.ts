baca8189e9bac85e93c442e4b18ebfcba99a9d84
leesangha <sanghalee1@naver.com>
2025-09-08 14:21:43 +0900

    solved: 217-contains-duplicates

---
+++ LeetCode/hash/217-contains-duplicates.ts
@@ -0,0 +1,9 @@
+function containsDuplicate(nums: number[]): boolean {
+  const map = new Map();
+  for (const num of nums) {
+    if (map.has(num)) return true;
+    map.set(num, true);
+  }
+
+  return false;
+}
