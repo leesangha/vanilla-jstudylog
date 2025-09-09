800512b0bce9851c1f0a2c9722f99a6b8e737ef3
leesangha <sanghalee1@naver.com>
2025-09-09 16:16:38 +0900

    solved: 104-maximum-depth-of-binary-tree

---
+++ LeetCode/tree/104-maximum-depth-of-binary-tree.ts
@@ -0,0 +1,8 @@
+function maxDepth(root: TreeNode | null): number {
+  if (!root) return 0;
+
+  const leftDepth = maxDepth(root.left);
+  const rightDepth = maxDepth(root.right);
+
+  return 1 + Math.max(leftDepth, rightDepth);
+}
